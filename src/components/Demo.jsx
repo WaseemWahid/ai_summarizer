8885ee4971913d1b40c2061dca0f875217dd93f3
Waseem <wahid.waseem@gmail.com>
2024-04-04 23:50:04 -0700

    new lines

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -17,7 +17,7 @@ const Demo = () => {
       <div className='flex flex-col w-full gap-2'>
         <form 
           className='relative flex justify-center items-center'
-          onSubmit={handleSubmit}
+          onSubmit={ handleSubmit }
         >
           <img 
             src={linkIcon}
@@ -46,6 +46,8 @@ const Demo = () => {
         </form>
 
         {/* Browse URL history */}
+
+
 
       </div>
 
